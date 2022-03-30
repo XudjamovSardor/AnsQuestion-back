14ee7301c470f531b475529d6af54300d21ca384
Sardor <xudjamovsardor12@gmail.com>
2022-03-30 22:23:09 +0500

    first commit

---
+++ src/test/java/net/idrok/ansquestion/AnsquestionApplicationTests.java
@@ -0,0 +1,13 @@
+package net.idrok.ansquestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsquestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
